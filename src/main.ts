097b70b05d1812b16744520c1faf0f725a41ad4b
damianGG <damiangolon@gmail.com>
2024-03-01 10:41:42 +0100

    Update app.listen to use dynamic port

--- src/main.ts
+++ src/main.ts
@@ -7,6 +7,7 @@ async function bootstrap() {
   dotenv.config();
   const app = await NestFactory.create(AppModule);
   app.enableCors();
+  const port = process.env.PORT || 3000;
   const config = new DocumentBuilder()
     .setTitle('PartyHub API')
     .setDescription('')
@@ -15,6 +16,6 @@ async function bootstrap() {
     .build();
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api', app, document);
-  await app.listen(3001);
+  await app.listen(port, '0.0.0.0');
 }
 bootstrap();
